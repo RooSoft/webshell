e5874337193a3ca4c225a0b0e6c2e903040d5b05
Marc Lacoursiere <marc@roosoft.com>
2020-10-02 11:59:35 -0400

    created HttpHandlers module

--- main.go
+++ main.go
@@ -1,73 +1,19 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"flag"
 	"log"
 	"net/http"
-
-	"webshell/lib/shell"
+	"webshell/lib/httpHandlers"
 )
 
-var pass *string
-
-type command struct {
-	Pass string
-	Cmd  string
-	Opt  string
-	Args string
-}
-
-func decodeRequest(r *http.Request) (command, error) {
-	var req command
-
-	err := json.NewDecoder(r.Body).Decode(&req)
-
-	if err != nil {
-		return req, errors.New("error parsing json payload")
-	}
-
-	return req, nil
-}
-
-func verifyPassword(givenPassword string, requiredPassword string) error {
-	if givenPassword != requiredPassword {
-		return errors.New("wrong password")
-	}
-
-	return nil
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	command, err := decodeRequest(r)
-
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-
-	err = verifyPassword(command.Pass, *pass)
-
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-
-	err = shell.ExecuteCommand(command.Cmd, command.Opt, command.Args, w)
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-}
-
 func main() {
-	pass = flag.String("pass", "mypass", "passwd")
+	httpHandlers.RequiredPassword = flag.String("pass", "mypass", "passwd")
 	addr := flag.String("addr", ":9090", "bind addr and port")
 
 	flag.Parse()
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", httpHandlers.Handler)
 
 	log.Printf("start http server\n")
 	log.Fatal(http.ListenAndServe(*addr, nil))
