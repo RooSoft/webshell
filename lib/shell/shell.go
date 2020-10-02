e917f3b68d39d8ebb332823510231ebdde0793ce
Marc Lacoursiere <marc@roosoft.com>
2020-10-02 13:01:42 -0400

    trap cmd.StderrPipe() and cmd.StdoutPipe errors, and add some comments

--- lib/shell/shell.go
+++ lib/shell/shell.go
@@ -7,15 +7,23 @@ import (
 	"os/exec"
 )
 
+// ExecuteCommand executes a command and sends output to the io.Writer
 func ExecuteCommand(command, options, arguments string, w io.Writer) error {
 	cmd := exec.Command(command, options, arguments)
 
-	stdoutIn, _ := cmd.StdoutPipe()
-	stderrIn, _ := cmd.StderrPipe()
+	stdoutIn, err := cmd.StdoutPipe()
+	if err != nil {
+		return errors.New("error creating stdout pipe")
+	}
+
+	stderrIn, err := cmd.StderrPipe()
+	if err != nil {
+		return errors.New("error creating stdout pipe")
+	}
 
 	var errStdout, errStderr error
 
-	err := cmd.Start()
+	err = cmd.Start()
 
 	if err != nil {
 		message := fmt.Sprintf("cmd.Start() failed with '%s'\n", err)
